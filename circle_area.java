6fb4fba6120864c734e86f671199fb10eedaa40b
Luan Batista <56078396+luanbatistadev@users.noreply.github.com>
2022-03-28 14:05:42 -0300

    java

--- circle_area.java
+++ circle_area.java
@@ -2,13 +2,14 @@ import java.io.IOException;
 import java.text.DecimalFormat;
 import java.util.Scanner;
 
-public class Main {
+public class circle_area {
  
     public static void main(String[] args) throws IOException {
         DecimalFormat formatar = new DecimalFormat("0.0000");
         double area, raio;
-        Scanner leia = new Scanner(System.in);
-        raio = leia.nextDouble();
+        try (Scanner leia = new Scanner(System.in)) {
+            raio = leia.nextDouble();
+        }
         area = 3.14159 * (raio * raio);
         System.out.println("A=" + formatar.format(area));
     }
--- extremely_basic.java
+++ extremely_basic.java
@@ -1,13 +1,14 @@
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Main {
+public class extremely_basic {
  
     public static void main(String[] args) throws IOException {
         int A, B, X;
-        Scanner read = new Scanner(System.in);
-        A = read.nextInt();
-        B = read.nextInt();
+        try (Scanner read = new Scanner(System.in)) {
+            A = read.nextInt();
+            B = read.nextInt();
+        }
         X = A + B;
         System.out.println("X = " + X);
     }
